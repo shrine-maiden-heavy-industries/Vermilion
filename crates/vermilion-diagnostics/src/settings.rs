c9b46946f04750942d07f6ee4ebe6e1bddce39cc
Aki Van Ness <aki@lethalbit.net>
2025-11-16 11:03:08 -0800

    Stubbed out `vermilion-diagnostics` crate

---
+++ crates/vermilion-diagnostics/src/settings.rs
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(
+	feature = "serde",
+	derive(::serde::Serialize, ::serde::Deserialize),
+	serde(deny_unknown_fields)
+)]
+#[cfg_attr(feature = "schema", derive(::schemars::JsonSchema))]
+pub struct CommonLint {}
