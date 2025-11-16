2160086d6697d0632340b08eae5184078da0c722
Aki Van Ness <aki@lethalbit.net>
2025-11-16 11:02:43 -0800

    Stubbed out `vermilion-core` crate

---
+++ crates/vermilion-core/src/lib.rs
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+#![cfg_attr(docsrs, feature(doc_cfg), deny(rustdoc::all))]
+#![doc = include_str!("../README.md")]
+// #![warn(missing_docs)]
+// #![warn(clippy::missing_docs_in_private_items)]
+
+pub mod settings;
+
+pub mod vars {
+	pub static VERMILION_LOG_LEVEL: &str = "VERMILION_LOG_LEVEL";
+	pub static VERMILION_CACHE_DIR: &str = "VERMILION_CACHE_DIR";
+}
