4a7897c507987a7e7c9f70b6f7467c7c1b003f79
Aki Van Ness <aki@lethalbit.net>
2025-11-16 11:03:41 -0800

    Stubbed out `vermilion-lsp` crate

---
+++ crates/vermilion-lsp/src/lib.rs
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+#![cfg_attr(docsrs, feature(doc_cfg), deny(rustdoc::all))]
+#![doc = include_str!("../README.md")]
+// #![warn(missing_docs)]
+// #![warn(clippy::missing_docs_in_private_items)]
+
+pub mod types;
