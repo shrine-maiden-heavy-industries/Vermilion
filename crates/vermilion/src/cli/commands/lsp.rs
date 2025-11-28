4d6219bccb012c2a273af80c5c723423970af4fe
Aki Van Ness <aki@lethalbit.net>
2025-11-27 19:24:14 -0800

    vermilion: lsp: Stubbed out spinning up the tokio runtime for the LSP

--- crates/vermilion/src/cli/commands/lsp.rs
+++ crates/vermilion/src/cli/commands/lsp.rs
@@ -1,6 +1,12 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 
+use std::{
+	sync::atomic::{AtomicUsize, Ordering},
+	time::Duration,
+};
+
 use clap::{ArgMatches, Command};
+use tracing::debug;
 
 use crate::settings::Config;
 
@@ -11,5 +17,22 @@ pub(crate) fn init() -> eyre::Result<Command> {
 }
 
 pub(crate) fn exec(_args: &ArgMatches, _cfg: Config) -> eyre::Result<()> {
+	debug!("Starting runtime...");
+	let rt = tokio::runtime::Builder::new_multi_thread()
+		.enable_all()
+		.worker_threads(4)
+		.thread_name_fn(|| {
+			static WORKER_POOL_ID: AtomicUsize = AtomicUsize::new(0);
+			let pool_id = WORKER_POOL_ID.fetch_add(1, Ordering::SeqCst);
+			format!("vermilion-worker-{pool_id}")
+		})
+		.on_thread_start(|| {
+			debug!("Starting tokio worker thread");
+		})
+		.build()?;
+
+	debug!("Shutting down runtime...");
+	rt.shutdown_timeout(Duration::from_secs(10));
+
 	Ok(())
 }
