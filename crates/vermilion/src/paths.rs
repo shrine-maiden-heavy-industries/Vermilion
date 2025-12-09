1bbf58c5bbdf9d3c41b761e307cb4d7097893a85
Aki Van Ness <aki@lethalbit.net>
2025-12-09 15:10:55 -0800

    vermilion: Added a paths helper to return if two paths are on the same FS
    This is only implemented for Unix-likes for now, because no-one actually uses Windows, it's just some obscure operating system.

--- crates/vermilion/src/paths.rs
+++ crates/vermilion/src/paths.rs
@@ -31,3 +31,19 @@ pub(crate) fn cache_dir() -> &'static Path {
 pub(crate) fn config_file() -> PathBuf {
 	config_dir().join("config.toml")
 }
+
+// When doing config lookup, we want to ensure that we stay on the same filesystem
+#[cfg(not(target_os = "windows"))]
+#[inline(always)]
+pub(crate) fn same_fs(path1: &PathBuf, path2: &PathBuf) -> eyre::Result<bool> {
+	use std::{fs, os::unix::fs::MetadataExt};
+
+	Ok(fs::metadata(path1)?.dev() == fs::metadata(path2)?.dev())
+}
+
+#[cfg(target_os = "windows")]
+#[inline(always)]
+pub(crate) fn same_fs(path1: &PathBuf, path2: &PathBuf) -> eyre::Result<bool> {
+	// TODO(aki): Figure this out, eventually, maybe, who even uses windows anyway?
+	todo!()
+}
