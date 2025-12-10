7dd18b46af2def322884331969df705503a08cf5
Aki Van Ness <aki@lethalbit.net>
2025-12-10 13:40:16 -0800

    vermilion: commands: Stubbed out the `init` command a little bit more

--- crates/vermilion/src/cli/commands/init.rs
+++ crates/vermilion/src/cli/commands/init.rs
@@ -1,15 +1,30 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 
-use clap::{Arg, ArgAction, ArgMatches, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command, ValueHint};
 
 use crate::settings::Config;
 
 pub(crate) const COMMAND_NAME: &str = "init";
 
 pub(crate) fn init() -> eyre::Result<Command> {
-	Ok(Command::new(COMMAND_NAME).about("Initialize a new Vermilion project or config"))
+	Ok(Command::new(COMMAND_NAME)
+		.about("Initialize vermilion in an existing HDL project or create a new project")
+		// .arg(
+		// 	Arg::new("name")
+		// 		.long("name")
+		// 		.help("The name of the project")
+		// 		.value_name("NAME")
+		// 		.action(ArgAction::Set),
+		// )
+		.arg(
+			Arg::new("path")
+				.help("The path of the project")
+				.action(ArgAction::Set)
+				.value_name("PATH")
+				.value_hint(ValueHint::DirPath)
+		))
 }
 
 pub(crate) fn exec(_args: &ArgMatches, _cfg: Config) -> eyre::Result<()> {
-	Ok(())
+	todo!("TODO(aki): Actually figure out how we want this to work")
 }
