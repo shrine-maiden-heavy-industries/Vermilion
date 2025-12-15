5832b4f220442795bea524f9ed0b6dcd6cc190c2
dragonmux <git@dragonmux.network>
2025-12-15 06:10:29 +0000

    verilog: lang: tokenizer: Implemented display and helpers for Position

--- crates/vermilion-verilog/src/lang/tokenizer/mod.rs
+++ crates/vermilion-verilog/src/lang/tokenizer/mod.rs
@@ -7,3 +7,20 @@ pub struct Position {
 	line: usize,
 	character: usize,
 }
+
+impl Position {
+	pub fn next_line(&mut self) {
+		self.character = 0;
+		self.line += 1;
+	}
+
+	pub fn next_char(&mut self) {
+		self.character += 1;
+	}
+}
+
+impl Display for Position {
+	fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(fmt, "line {}, character {}", self.line, self.character)
+	}
+}
