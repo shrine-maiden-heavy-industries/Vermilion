58e37bdbdacddac1cec023a01456c95560fda73f
dragonmux <git@dragonmux.network>
2025-12-15 07:12:55 +0000

    verilog: parser: Provide a way to construct a parser from a standard and some maybe-Verilog so the Tokenizer is used

--- crates/vermilion-verilog/src/lang/parser.rs
+++ crates/vermilion-verilog/src/lang/parser.rs
@@ -1,9 +1,19 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 
-use crate::VerilogVariant;
+use crate::{VerilogVariant, lang::tokenizer::Tokenizer};
 
 pub struct VerilogParser {
 	std: VerilogVariant,
+	tokenizer: Tokenizer,
+}
+
+impl VerilogParser {
+	pub fn new(std: VerilogVariant, content: &str) -> Self {
+		Self {
+			std,
+			tokenizer: Tokenizer::new(std, content.as_bytes().into())
+		}
+	}
 }
 
 #[cfg(test)]
