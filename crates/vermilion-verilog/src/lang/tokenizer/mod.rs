a34d576bc8106a7bec9f4721abc1f507f1fd1de3
Aki Van Ness <aki@lethalbit.net>
2025-12-15 19:00:09 -0800

    verilog: lang: tokenizer: Cleaned up the tokenizer tests some more

--- crates/vermilion-verilog/src/lang/tokenizer/mod.rs
+++ crates/vermilion-verilog/src/lang/tokenizer/mod.rs
@@ -832,328 +832,332 @@ mod tests {
 	use super::*;
 	use crate::VerilogStd;
 
-	macro_rules! assert_tokenized {
-		($string:literal, $tokens:expr) => {{
-			let tokenizer = Tokenizer::new(
-				VerilogVariant::Verilog(VerilogStd::Vl95),
-				$string.as_bytes().into(),
-			);
+	macro_rules! tokenizer_test {
+		($test_name:ident, $input:literal, $tokens:expr) => {
+			#[test]
+			fn $test_name() {
+				let tokenizer = Tokenizer::new(
+					VerilogVariant::Verilog(VerilogStd::Vl95),
+					$input.as_bytes().into(),
+				);
 
-			let parsed = tokenizer.collect::<Vec<_>>();
-			let expected = $tokens;
+				let parsed = tokenizer.collect::<Vec<_>>();
+				let expected = $tokens;
 
-			assert_eq!(
-				parsed, expected,
-				"Parsed: {parsed:#?}\nExpected: {expected:#?}"
-			);
-		}};
+				assert_eq!(
+					parsed, expected,
+					"Parsed: {parsed:#?}\nExpected: {expected:#?}"
+				);
+			}
+		};
 	}
 
-	#[test]
-	fn test_tokenize_whitespace() {
-		assert_tokenized!(
-			" \r\n \t\n\t\n\r",
-			vec![
-				Spanned::new(
-					Token::Whitespace(" ".as_bytes().into()),
-					Some(Span::new(0..1, Position::new(0, 0))),
-				),
-				Spanned::new(
-					Token::Newline("\r\n".as_bytes().into()),
-					Some(Span::new(1..3, Position::new(0, 1))),
-				),
-				Spanned::new(
-					Token::Whitespace(" \t".as_bytes().into()),
-					Some(Span::new(3..5, Position::new(1, 0))),
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(5..6, Position::new(1, 2))),
-				),
-				Spanned::new(
-					Token::Whitespace("\t".as_bytes().into()),
-					Some(Span::new(6..7, Position::new(2, 0))),
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(7..8, Position::new(2, 1))),
-				),
-				Spanned::new(
-					Token::Newline("\r".as_bytes().into()),
-					Some(Span::new(8..9, Position::new(3, 0))),
-				),
-			]
-		);
-	}
+	tokenizer_test!(
+		test_tokenize_whitespace,
+		" \r\n \t\n\t\n\r",
+		vec![
+			Spanned::new(
+				Token::Whitespace(" ".as_bytes().into()),
+				Some(Span::new(0..1, Position::new(0, 0))),
+			),
+			Spanned::new(
+				Token::Newline("\r\n".as_bytes().into()),
+				Some(Span::new(1..3, Position::new(0, 1))),
+			),
+			Spanned::new(
+				Token::Whitespace(" \t".as_bytes().into()),
+				Some(Span::new(3..5, Position::new(1, 0))),
+			),
+			Spanned::new(
+				Token::Newline("\n".as_bytes().into()),
+				Some(Span::new(5..6, Position::new(1, 2))),
+			),
+			Spanned::new(
+				Token::Whitespace("\t".as_bytes().into()),
+				Some(Span::new(6..7, Position::new(2, 0))),
+			),
+			Spanned::new(
+				Token::Newline("\n".as_bytes().into()),
+				Some(Span::new(7..8, Position::new(2, 1))),
+			),
+			Spanned::new(
+				Token::Newline("\r".as_bytes().into()),
+				Some(Span::new(8..9, Position::new(3, 0))),
+			),
+		]
+	);
 
-	#[test]
-	fn test_tilde_operators() {
-		assert_tokenized!(
-			"~",
-			vec![Spanned::new(
-				Token::Operator(Operator::Tilde),
+	tokenizer_test!(
+		test_tokenize_tilde,
+		"~",
+		vec![Spanned::new(
+			Token::Operator(Operator::Tilde),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_tilde_circumflex_right,
+		"~^",
+		vec![Spanned::new(
+			Token::Operator(Operator::TildeCircumflex(false)),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_reduction_nand,
+		"~&",
+		vec![Spanned::new(
+			Token::Operator(Operator::ReductionNand),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_reduction_nor,
+		"~|",
+		vec![Spanned::new(
+			Token::Operator(Operator::ReductionNor),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_circumflex,
+		"^",
+		vec![Spanned::new(
+			Token::Operator(Operator::Circumflex),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_tilde_circumflex_left,
+		"^~",
+		vec![Spanned::new(
+			Token::Operator(Operator::TildeCircumflex(true)),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_pipe,
+		"|",
+		vec![Spanned::new(
+			Token::Operator(Operator::Pipe),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_logical_or,
+		"||",
+		vec![Spanned::new(
+			Token::Operator(Operator::LogicalOr),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_ampersand,
+		"&",
+		vec![Spanned::new(
+			Token::Operator(Operator::Ampersand),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_logical_and,
+		"&&",
+		vec![Spanned::new(
+			Token::Operator(Operator::LogicalAnd),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_less_than,
+		">",
+		vec![Spanned::new(
+			Token::Operator(Operator::LessThan),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_less_than_equal,
+		">=",
+		vec![Spanned::new(
+			Token::Operator(Operator::LessThanEqual),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_shift_right,
+		">>",
+		vec![Spanned::new(
+			Token::Operator(Operator::ShiftRight),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_greater_than,
+		"<",
+		vec![Spanned::new(
+			Token::Operator(Operator::GreaterThan),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_greater_than_equal,
+		"<=",
+		vec![Spanned::new(
+			Token::Operator(Operator::GreaterThanEqual),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_shift_left,
+		"<<",
+		vec![Spanned::new(
+			Token::Operator(Operator::ShiftLeft),
+			Some(Span::new(0..2, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_solidus,
+		"/",
+		vec![Spanned::new(
+			Token::Operator(Operator::Solidus),
+			Some(Span::new(0..1, Position::new(0, 0)))
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_equal,
+		"=",
+		vec![Spanned::new(
+			Token::Operator(Operator::Equals),
+			Some(Span::new(0..1, Position::new(0, 0))),
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_logical_equality,
+		"==",
+		vec![Spanned::new(
+			Token::Operator(Operator::LogicalEquality),
+			Some(Span::new(0..2, Position::new(0, 0))),
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_case_equality,
+		"===",
+		vec![Spanned::new(
+			Token::Operator(Operator::CaseEquality),
+			Some(Span::new(0..3, Position::new(0, 0))),
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_exclamation,
+		"!",
+		vec![Spanned::new(
+			Token::Operator(Operator::Exclamation),
+			Some(Span::new(0..1, Position::new(0, 0))),
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_logical_inequality,
+		"!=",
+		vec![Spanned::new(
+			Token::Operator(Operator::LogicalInequality),
+			Some(Span::new(0..2, Position::new(0, 0))),
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_case_inequality,
+		"!==",
+		vec![Spanned::new(
+			Token::Operator(Operator::CaseInequality),
+			Some(Span::new(0..3, Position::new(0, 0))),
+		)]
+	);
+
+	tokenizer_test!(
+		test_tokenize_binary_lump,
+		"4 'b01zx\n+1'B ?\n2'bZX\n",
+		vec![
+			Spanned::new(
+				Token::UnsignedNumber("4".as_bytes().into()),
 				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"~^",
-			vec![Spanned::new(
-				Token::Operator(Operator::TildeCircumflex(false)),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"~&",
-			vec![Spanned::new(
-				Token::Operator(Operator::ReductionNand),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"~|",
-			vec![Spanned::new(
-				Token::Operator(Operator::ReductionNor),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_circumflex_operators() {
-		assert_tokenized!(
-			"^",
-			vec![Spanned::new(
-				Token::Operator(Operator::Circumflex),
-				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"^~",
-			vec![Spanned::new(
-				Token::Operator(Operator::TildeCircumflex(true)),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_pipe_operators() {
-		assert_tokenized!(
-			"|",
-			vec![Spanned::new(
-				Token::Operator(Operator::Pipe),
-				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"||",
-			vec![Spanned::new(
-				Token::Operator(Operator::LogicalOr),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_ampersand_operators() {
-		assert_tokenized!(
-			"&",
-			vec![Spanned::new(
-				Token::Operator(Operator::Ampersand),
-				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"&&",
-			vec![Spanned::new(
-				Token::Operator(Operator::LogicalAnd),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_less_than_operators() {
-		assert_tokenized!(
-			">",
-			vec![Spanned::new(
-				Token::Operator(Operator::LessThan),
-				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			">=",
-			vec![Spanned::new(
-				Token::Operator(Operator::LessThanEqual),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			">>",
-			vec![Spanned::new(
-				Token::Operator(Operator::ShiftRight),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_greater_than_operators() {
-		assert_tokenized!(
-			"<",
-			vec![Spanned::new(
-				Token::Operator(Operator::GreaterThan),
-				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"<=",
-			vec![Spanned::new(
-				Token::Operator(Operator::GreaterThanEqual),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-
-		assert_tokenized!(
-			"<<",
-			vec![Spanned::new(
-				Token::Operator(Operator::ShiftLeft),
-				Some(Span::new(0..2, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_solidus_operators() {
-		assert_tokenized!(
-			"/",
-			vec![Spanned::new(
-				Token::Operator(Operator::Solidus),
-				Some(Span::new(0..1, Position::new(0, 0)))
-			)]
-		);
-	}
-
-	#[test]
-	fn test_equality_operators() {
-		assert_tokenized!(
-			"!= \n===\t!==\n\n==",
-			vec![
-				Spanned::new(
-					Token::Operator(Operator::LogicalInequality),
-					Some(Span::new(0..2, Position::new(0, 0))),
-				),
-				Spanned::new(
-					Token::Whitespace(" ".as_bytes().into()),
-					Some(Span::new(2..3, Position::new(0, 2))),
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(3..4, Position::new(0, 3))),
-				),
-				Spanned::new(
-					Token::Operator(Operator::CaseEquality),
-					Some(Span::new(4..7, Position::new(1, 0))),
-				),
-				Spanned::new(
-					Token::Whitespace("\t".as_bytes().into()),
-					Some(Span::new(7..8, Position::new(1, 3))),
-				),
-				Spanned::new(
-					Token::Operator(Operator::CaseInequality),
-					Some(Span::new(8..11, Position::new(1, 4))),
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(11..12, Position::new(1, 7))),
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(12..13, Position::new(2, 0))),
-				),
-				Spanned::new(
-					Token::Operator(Operator::LogicalEquality),
-					Some(Span::new(13..15, Position::new(3, 0))),
-				),
-			]
-		);
-	}
-
-	#[test]
-	fn test_tokenize_binary_number() {
-		assert_tokenized!(
-			"4 'b01zx\n+1'B ?\n2'bZX\n",
-			vec![
-				Spanned::new(
-					Token::UnsignedNumber("4".as_bytes().into()),
-					Some(Span::new(0..1, Position::new(0, 0)))
-				),
-				Spanned::new(
-					Token::Whitespace(" ".as_bytes().into()),
-					Some(Span::new(1..2, Position::new(0, 1)))
-				),
-				Spanned::new(
-					Token::BaseSpecifier(token::BaseSpecifier::Binary, false),
-					Some(Span::new(2..4, Position::new(0, 2)))
-				),
-				Spanned::new(
-					Token::Number("01zx".as_bytes().into()),
-					Some(Span::new(4..8, Position::new(0, 4)))
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(8..9, Position::new(0, 8)))
-				),
-				Spanned::new(
-					Token::Sign(token::Sign::Positive),
-					Some(Span::new(9..10, Position::new(1, 0)))
-				),
-				Spanned::new(
-					Token::UnsignedNumber("1".as_bytes().into()),
-					Some(Span::new(10..11, Position::new(1, 1)))
-				),
-				Spanned::new(
-					Token::BaseSpecifier(token::BaseSpecifier::Binary, true),
-					Some(Span::new(11..13, Position::new(1, 2)))
-				),
-				Spanned::new(
-					Token::Whitespace(" ".as_bytes().into()),
-					Some(Span::new(13..14, Position::new(1, 4)))
-				),
-				Spanned::new(
-					Token::Number("?".as_bytes().into()),
-					Some(Span::new(14..15, Position::new(1, 5)))
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(15..16, Position::new(1, 6)))
-				),
-				Spanned::new(
-					Token::UnsignedNumber("2".as_bytes().into()),
-					Some(Span::new(16..17, Position::new(2, 0)))
-				),
-				Spanned::new(
-					Token::BaseSpecifier(token::BaseSpecifier::Binary, false),
-					Some(Span::new(17..19, Position::new(2, 1)))
-				),
-				Spanned::new(
-					Token::Number("ZX".as_bytes().into()),
-					Some(Span::new(19..21, Position::new(2, 3)))
-				),
-				Spanned::new(
-					Token::Newline("\n".as_bytes().into()),
-					Some(Span::new(21..22, Position::new(2, 5)))
-				),
-			]
-		);
-	}
+			),
+			Spanned::new(
+				Token::Whitespace(" ".as_bytes().into()),
+				Some(Span::new(1..2, Position::new(0, 1)))
+			),
+			Spanned::new(
+				Token::BaseSpecifier(token::BaseSpecifier::Binary, false),
+				Some(Span::new(2..4, Position::new(0, 2)))
+			),
+			Spanned::new(
+				Token::Number("01zx".as_bytes().into()),
+				Some(Span::new(4..8, Position::new(0, 4)))
+			),
+			Spanned::new(
+				Token::Newline("\n".as_bytes().into()),
+				Some(Span::new(8..9, Position::new(0, 8)))
+			),
+			Spanned::new(
+				Token::Sign(token::Sign::Positive),
+				Some(Span::new(9..10, Position::new(1, 0)))
+			),
+			Spanned::new(
+				Token::UnsignedNumber("1".as_bytes().into()),
+				Some(Span::new(10..11, Position::new(1, 1)))
+			),
+			Spanned::new(
+				Token::BaseSpecifier(token::BaseSpecifier::Binary, true),
+				Some(Span::new(11..13, Position::new(1, 2)))
+			),
+			Spanned::new(
+				Token::Whitespace(" ".as_bytes().into()),
+				Some(Span::new(13..14, Position::new(1, 4)))
+			),
+			Spanned::new(
+				Token::Number("?".as_bytes().into()),
+				Some(Span::new(14..15, Position::new(1, 5)))
+			),
+			Spanned::new(
+				Token::Newline("\n".as_bytes().into()),
+				Some(Span::new(15..16, Position::new(1, 6)))
+			),
+			Spanned::new(
+				Token::UnsignedNumber("2".as_bytes().into()),
+				Some(Span::new(16..17, Position::new(2, 0)))
+			),
+			Spanned::new(
+				Token::BaseSpecifier(token::BaseSpecifier::Binary, false),
+				Some(Span::new(17..19, Position::new(2, 1)))
+			),
+			Spanned::new(
+				Token::Number("ZX".as_bytes().into()),
+				Some(Span::new(19..21, Position::new(2, 3)))
+			),
+			Spanned::new(
+				Token::Newline("\n".as_bytes().into()),
+				Some(Span::new(21..22, Position::new(2, 5)))
+			),
+		]
+	);
 }
