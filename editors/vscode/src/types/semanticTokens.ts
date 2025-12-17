a33d27ab79185b67ab336f6ed9a448c470f3329c
dragonmux <git@dragonmux.network>
2025-12-16 19:38:08 -0800

    editors: vscode: types: Implemented types for handling semantic tokens from the language server

---
+++ editors/vscode/src/types/semanticTokens.ts
@@ -0,0 +1,35 @@
+import {Diagnostic} from 'vscode-languageclient'
+
+export enum SemanticTokenTypes
+{
+	type = 0,
+	class = 1,
+	enumMember = 2,
+	parameter = 3,
+	variable = 4,
+	function = 5,
+	operator = 6,
+	keyword = 7,
+	comment = 8,
+	string = 9,
+	number = 10,
+	templateType = 11,
+	templateFunction = 12,
+	const = 13
+}
+
+export interface SemanticToken
+{
+	line: number
+	character: number
+	length: number
+	type: SemanticTokenTypes
+	modifiers?: number
+}
+
+export interface GetSemanticTokensResult
+{
+	canceled: boolean
+	tokens: SemanticToken[]
+	diagnostics: Diagnostic[]
+}
