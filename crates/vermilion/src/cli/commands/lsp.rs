370b38296aba58548aace0cf7a6065a58f3c5915
Aki Van Ness <aki@lethalbit.net>
2025-12-16 23:36:13 -0800

    vermilion: lsp: Added CLI options for language server command

--- crates/vermilion/src/cli/commands/lsp.rs
+++ crates/vermilion/src/cli/commands/lsp.rs
@@ -1,19 +1,55 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 
 use std::{
+	path::PathBuf,
 	sync::atomic::{AtomicUsize, Ordering},
 	time::Duration,
 };
 
-use clap::{ArgMatches, Command};
-use tracing::debug;
+use clap::{Arg, ArgAction, ArgMatches, Command, ValueHint};
 
 use crate::settings::Config;
 
 pub(crate) const COMMAND_NAME: &str = "lang-server";
 
 pub(crate) fn init() -> eyre::Result<Command> {
-	Ok(Command::new(COMMAND_NAME).about("Start the Vermillion LSP server"))
+	Ok(Command::new(COMMAND_NAME)
+		.about("Start the Vermillion LSP server")
+		.arg(
+			Arg::new("pipe")
+				.long("pipe")
+				.help("Path to the Unix socket or named pipe for LSP communication")
+				.value_hint(ValueHint::FilePath)
+				.value_name("PIPE")
+				.value_parser(clap::value_parser!(PathBuf))
+				.action(ArgAction::Set)
+				// .conflicts_with_all(["socket", "stdio"]),
+		)
+		// XXX(aki): For now we're only supporting LSP over PIPE,
+		// .arg(
+		// 	Arg::new("socket")
+		// 		.long("socket")
+		// 		.help("The port to connect to for the LSP server to talk to")
+		// 		.value_name("PORT")
+		// 		.value_parser(clap::value_parser!(u16))
+		// 		.action(ArgAction::Set)
+		// 		.conflicts_with_all(["pipe", "stdio"]),
+		// )
+		// .arg(
+		// 	Arg::new("stdio")
+		// 		.long("stdio")
+		// 		.help("Use STDIO for LSP communication")
+		// 		.action(ArgAction::SetTrue)
+		// 		.conflicts_with_all(["pipe", "socket"]),
+		// )
+		.arg(
+			Arg::new("client-pid")
+				.long("clientProcessId")
+				.help("The LSP client's PID")
+				.value_name("PID")
+				.value_parser(clap::value_parser!(usize))
+				.action(ArgAction::Set),
+		))
 }
 
 pub(crate) fn exec(_args: &ArgMatches, _cfg: Config) -> eyre::Result<()> {
