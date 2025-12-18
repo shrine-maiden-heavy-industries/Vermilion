35f6d13252b979ccc15ce6806af926a8927f9268
Aki Van Ness <aki@lethalbit.net>
2025-12-18 05:30:55 -0800

    editors: vscode: Stubbed out some configuration bits

---
+++ editors/vscode/src/config.ts
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+export class Config {
+	readonly extensionId = "shrine-maiden-heavy-industries.vermilion";
+
+	// TODO(aki): Actually deal with configuration stuff
+}
