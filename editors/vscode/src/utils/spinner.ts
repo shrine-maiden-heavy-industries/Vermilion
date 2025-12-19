0ad194e17bf8ffc8cccb77db02719cbe2603b5f7
dragonmux <git@dragonmux.network>
2025-12-19 03:04:45 +0000

    editors: vscode: utils: Correct a copy-pasta error for the spinner status indicator block

--- editors/vscode/src/utils/spinner.ts
+++ editors/vscode/src/utils/spinner.ts
@@ -4,11 +4,11 @@ import {window} from 'vscode'
 /* exported startSpinner */
 export function startSpinner(message: string)
 {
-	window.setStatusBarMessage(`Mangrove: $(settings-gear~spin) ${message}`)
+	window.setStatusBarMessage(`Vermilion: $(settings-gear~spin) ${message}`)
 }
 
 /* exported stopSpinner */
 export function stopSpinner(message?: string)
 {
-	window.setStatusBarMessage(message ? `Mangrove: ${message}` : 'Mangrove')
+	window.setStatusBarMessage(message ? `Vermilion: ${message}` : 'Vermilion')
 }
