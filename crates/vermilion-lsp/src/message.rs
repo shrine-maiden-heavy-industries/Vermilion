7fb994279dee8361e22bd36d68a7ca882244521f
Aki Van Ness <aki@lethalbit.net>
2025-12-26 03:07:27 -0800

    lsp: message: Implement the rough serdes for the LSP messages

--- crates/vermilion-lsp/src/message.rs
+++ crates/vermilion-lsp/src/message.rs
@@ -2,6 +2,8 @@
 
 use std::fmt::{Debug, Display};
 
+use eyre::Result;
+
 use crate::{notification::Notification, request::Request, response::Response};
 
 /// A JSON-RPC message ID
@@ -22,7 +24,23 @@ pub enum Message {
 	Notification(Notification),
 }
 
-impl Message {}
+#[derive(Clone, Debug, serde::Serialize)]
+struct JsonRpcEnvelope<'a> {
+	jsonrpc: &'static str,
+	#[serde(flatten)]
+	message: &'a Message,
+}
+
+impl Message {
+	pub fn deserialize(buffer: &[u8]) -> Result<Message> {
+		Ok(serde_json::from_slice(buffer)?)
+	}
+
+	pub fn serialize(self, buffer: &mut [u8]) -> Result<usize> {
+		serde_json::to_writer(buffer, &JsonRpcEnvelope { jsonrpc: "2.0", message: &self })?;
+		Ok(0)
+	}
+}
 
 impl From<i32> for Id {
 	fn from(value: i32) -> Self {
