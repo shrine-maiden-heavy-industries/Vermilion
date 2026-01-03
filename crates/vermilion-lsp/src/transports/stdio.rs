0e95e9f703c4843da9f8058d520a819f77745027
Aki Van Ness <aki@lethalbit.net>
2026-01-03 13:13:59 -0800

    lsp: transport: Pulled message parsing logic into a standalone function, replaced the stdio usage with said function  to test,

--- crates/vermilion-lsp/src/transports/stdio.rs
+++ crates/vermilion-lsp/src/transports/stdio.rs
@@ -8,12 +8,12 @@ use tokio::{
 	task::JoinSet,
 };
 use tokio_util::sync::CancellationToken;
-use tracing::{error, trace};
+use tracing::error;
 
 use super::LSPTransport;
 use crate::{
 	message::Message,
-	transports::{ReadPhase, get_split_index},
+	transports::{ReadPhase, parse_message},
 };
 
 #[derive(Debug)]
@@ -42,102 +42,9 @@ async fn stdio_reader(
 			read_result = stdin.read(&mut buf) => {
 				match read_result {
 					Ok(read) => {
-						match phase {
-							ReadPhase::Header => {
-								let res = &buf[0..read];
-								// Check to see if what we read in starts with the content length header
-								if res.starts_with(b"Content-Length: ") {
-									// If the data read ends with an `\r\n` then we can kinda assume it's just the header
-									if res.ends_with(b"\r\n") {
-										let size = str::from_utf8(&res[16..])?.trim().parse::<usize>()?;
-										trace!("Got content header of len: {}", size);
-										phase = ReadPhase::Content(size);
-										continue;
-									} else {
-										// We read way more than just the header, so we need to deal with it specially
-										let pos = get_split_index(res);
-										if let Some(pos) = pos {
-											// Split out the header and body
-											let header = &res[16..pos];
-											let body = &res[pos..];
-
-											// Like above, extract the content length from the header
-											let size = match str::from_utf8(header) {
-												Ok(size) => {
-													match size.trim().parse::<usize>() {
-														Ok(size) => size,
-														Err(err) => {
-															error!("{}", err);
-															shutdown_channel.send(())?;
-															return Err(err.into());
-														}
-													}
-												},
-												Err(err) => {
-													error!("{}", err);
-													shutdown_channel.send(())?;
-													return Err(err.into());
-												}
-											};
-											trace!("Got content header of len: {}", size);
-
-											// Find out how many bytes we got vs how many left to read
-											let current_content = read - pos;
-											let remaining = size - current_content;
-
-											// Append the bytes we do have,
-											content.extend_from_slice(body);
-
-											// Only move on if we have more bytes to read
-											if remaining > 0 {
-												trace!("Have {} bytes of body already, {} remaining", current_content, remaining);
-												phase = ReadPhase::Content(remaining);
-											} else {
-												// Otherwise deserialize message and clear the buffer
-												match Message::deserialize(&content) {
-													Ok(msg) => sender.send(msg)?,
-													Err(e) => {
-														error!("Unable to deserialize LSP message:");
-														error!("{}", e);
-														error!("Message contents:");
-														error!("{}", str::from_utf8(&content)?);
-													}
-												}
-												content.clear();
-											}
-										} else {
-											error!("Unable to separate header from content!");
-											break;
-										}
-									}
-								} else {
-									error!("Invalid JSON-RPC header {:?}", str::from_utf8(res)?);
-									break;
-								}
-							},
-							ReadPhase::Content(length) => {
-								let remaining = length - read;
-								content.extend_from_slice(&buf[0..read]);
-
-								trace!("{} bytes remaining", remaining);
-								if remaining > 0 {
-									phase = ReadPhase::Content(remaining);
-								} else {
-									phase = ReadPhase::Header;
-									// Deserialize message
-									match Message::deserialize(&content) {
-										Ok(msg) => sender.send(msg)?,
-										Err(e) => {
-											error!("Unable to deserialize LSP message:");
-											error!("{}", e);
-											error!("Message contents:");
-											error!("{}", str::from_utf8(&content)?);
-										}
-									}
-									// Clear the contents buffer
-									content.clear();
-								}
-							},
+						match parse_message(read, &buf, &mut content, &mut phase, &sender, &shutdown_channel) {
+							Ok(_) => { continue; }
+							Err(_) => { break; }
 						}
 					},
 					Err(e) => {
