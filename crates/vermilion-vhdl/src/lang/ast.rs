01b576fadb37be065e10fd4bb240c6e3f167d27d
Aki Van Ness <aki@lethalbit.net>
2026-01-02 21:30:25 -0800

    vhdl: lang: formatting

--- crates/vermilion-vhdl/src/lang/ast.rs
+++ crates/vermilion-vhdl/src/lang/ast.rs
@@ -1,11 +1,11 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
+// SPDX-License-Identifier: BSD-3-Clause
 
 #[derive(Debug, Default)]
-pub struct Ast {
-}
+pub struct Ast {}
 
 #[cfg(test)]
 mod tests {
-	use super::*;
 	use test_log::test;
+
+	use super::*;
 }
