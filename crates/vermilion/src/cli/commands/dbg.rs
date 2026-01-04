c2262ca1d6c1907a7c7839b58162356e9e5107ba
Aki Van Ness <aki@lethalbit.net>
2026-01-03 18:48:17 -0800

    vermilion: cli: commands: dbg: Added `dump-tokens` command

--- crates/vermilion/src/cli/commands/dbg.rs
+++ crates/vermilion/src/cli/commands/dbg.rs
@@ -1,13 +1,26 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-use clap::{Arg, ArgAction, ArgMatches, Command};
+use std::{fs::File, io::Read};
 
-use crate::settings::Config;
+use clap::{Arg, ArgAction, ArgMatches, Command};
+use eyre::{OptionExt, eyre};
+
+use crate::{lang::Language, settings::Config};
 
 pub(crate) const COMMAND_NAME: &str = "dbg";
 
 fn subcommands() -> Vec<Command> {
 	vec![
+		Command::new("dump-tokens")
+			.about("Dump the token stream from the given file to stdout")
+			.arg(
+				Arg::new("files")
+					.help("HDL files")
+					.action(ArgAction::Append)
+					.value_name("FILE")
+					.value_delimiter(',')
+					.required(true),
+			),
 		Command::new("dump-ast")
 			.about("Dump a pretty-printed version of the AST to the command line")
 			.arg(
@@ -27,8 +40,45 @@ pub(crate) fn init() -> eyre::Result<Command> {
 		.subcommands(subcommands()))
 }
 
-pub(crate) fn exec(args: &ArgMatches, _cfg: Config) -> eyre::Result<()> {
-	let _lang_id = crate::lang::get_langid(args);
+pub(crate) fn exec(args: &ArgMatches, cfg: Config) -> eyre::Result<()> {
+	let lang_id = crate::lang::get_langid(args).ok_or_eyre("Unable to get language id")?;
+
+	match args.subcommand() {
+		Some((cmd, cmd_args)) => match cmd {
+			"dump-ast" => dump_ast(cmd_args, cfg, lang_id),
+			"dump-tokens" => dump_tokens(cmd_args, cfg, lang_id),
+			_ => unreachable!(),
+		},
+		_ => Err(eyre!("No subcommand")),
+	}
+}
+
+fn dump_ast(args: &ArgMatches, _cfg: Config, language: Language) -> eyre::Result<()> {
+	Ok(())
+}
+
+fn dump_tokens(args: &ArgMatches, _cfg: Config, language: Language) -> eyre::Result<()> {
+	fn dump_iter<I: Iterator>(itr: I)
+	where
+		<I as std::iter::Iterator>::Item: std::fmt::Display,
+	{
+		for tok in itr {
+			println!("{tok}");
+		}
+	}
+
+	if let Some(files) = args.try_get_many::<String>("files")? {
+		for file in files {
+			let mut data = Vec::new();
+			let mut hdl_file = File::open(file)?;
+			hdl_file.read_to_end(&mut data)?;
+
+			match language.tokenizer(data.as_slice().into()) {
+				crate::lang::Tokenizer::Verilog(tokenizer) => dump_iter(tokenizer),
+				crate::lang::Tokenizer::Vhdl(tokenizer) => dump_iter(tokenizer),
+			}
+		}
+	}
 
 	Ok(())
 }
