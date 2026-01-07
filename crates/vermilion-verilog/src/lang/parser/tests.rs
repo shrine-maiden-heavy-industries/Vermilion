04e11a3dbee570b43886f475bbbf085cada2cff8
Aki Van Ness <aki@lethalbit.net>
2026-01-06 21:44:34 -0800

    verilog: parser: Split out tests into by-standard modules

--- crates/vermilion-verilog/src/lang/parser/tests.rs
+++ crates/vermilion-verilog/src/lang/parser/tests.rs
@@ -1,1 +1,16 @@
 // SPDX-License-Identifier: BSD-3-Clause
+
+// Verilog Parser Tests
+include!("./tests/verilog95.rs");
+include!("./tests/verilog05.rs");
+include!("./tests/verilog01.rs");
+// SystemVerilog Parser Tests
+include!("./tests/system_verilog05.rs");
+include!("./tests/system_verilog09.rs");
+include!("./tests/system_verilog12.rs");
+include!("./tests/system_verilog17.rs");
+include!("./tests/system_verilog23.rs");
+// Verilog-AMS Parser Tests
+include!("./tests/verilog_ams09.rs");
+include!("./tests/verilog_ams14.rs");
+include!("./tests/verilog_ams23.rs");
