cdfe6d9a10a87ba2fb9fcbfea3e009e938bae7ea
Aki Van Ness <aki@lethalbit.net>
2026-01-06 21:43:08 -0800

    vhdl: parser: split out tests into version specific modules

--- crates/vermilion-vhdl/src/lang/parser/tests.rs
+++ crates/vermilion-vhdl/src/lang/parser/tests.rs
@@ -1,1 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
+
+include!("./tests/vhdl87.rs");
+include!("./tests/vhdl93.rs");
+include!("./tests/vhdl2k.rs");
+include!("./tests/vhdl02.rs");
+include!("./tests/vhdl07.rs");
+include!("./tests/vhdl08.rs");
+include!("./tests/vhdl11.rs");
+include!("./tests/vhdl19.rs");
+include!("./tests/vhdl23.rs");
