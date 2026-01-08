7fcc29abb9f6f7e8decd6d85991e41247a5b90c2
Aki Van Ness <aki@lethalbit.net>
2026-01-08 00:10:32 -0800

    verilog: tokenizer: tests: Added test module for common tokenizer tests

--- crates/vermilion-verilog/src/lang/tokenizer/tests.rs
+++ crates/vermilion-verilog/src/lang/tokenizer/tests.rs
@@ -171,17 +171,22 @@ macro_rules! all_tokenizer_test {
 	};
 }
 
+// Common Tokenizer Tests
+include!("./tests/all_common.rs");
 // Verilog Tokenizer Tests
+include!("./tests/verilog_common.rs");
 include!("./tests/verilog95.rs");
 include!("./tests/verilog05.rs");
 include!("./tests/verilog01.rs");
 // SystemVerilog Tokenizer Tests
+include!("./tests/system_verilog_common.rs");
 include!("./tests/system_verilog05.rs");
 include!("./tests/system_verilog09.rs");
 include!("./tests/system_verilog12.rs");
 include!("./tests/system_verilog17.rs");
 include!("./tests/system_verilog23.rs");
 // Verilog-AMS Tokenizer Tests
+include!("./tests/verilog_ams_common.rs");
 include!("./tests/verilog_ams09.rs");
 include!("./tests/verilog_ams14.rs");
 include!("./tests/verilog_ams23.rs");
