a87b809de63e19ed6366ee4f61a70f1b7a6f5c81
Aki Van Ness <aki@lethalbit.net>
2026-01-10 22:04:05 -0800

    vermilion: env: Added `#[allow(unused)]` to the cache dir env var

--- crates/vermilion/src/env.rs
+++ crates/vermilion/src/env.rs
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 pub static VERMILION_LOG_LEVEL: &str = "VERMILION_LOG_LEVEL";
+#[allow(unused)]
 pub static VERMILION_CACHE_DIR: &str = "VERMILION_CACHE_DIR";
