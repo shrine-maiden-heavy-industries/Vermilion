8ce6f7b81a8f71a7074b76648f10886a755fe19b
Aki Van Ness <aki@lethalbit.net>
2026-01-16 04:05:05 -0800

    verilog: lang: wiggled some AST bits

--- crates/vermilion-verilog/src/lang/ast.rs
+++ crates/vermilion-verilog/src/lang/ast.rs
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+use std::collections::HashMap;
+
+use vermilion_lang::{AtomicByteTendril, Position, Spanned};
+
+use crate::lang::tokenizer::token::{Comment, CompilerDirective};
+
+#[derive(Debug)]
+pub struct Module {
+	pub name: AtomicByteTendril,
+}
+
+#[derive(Debug)]
+pub struct Primitive {
+	pub name: AtomicByteTendril,
+}
+
 #[derive(Debug, Default)]
-pub struct Ast {}
+pub struct Ast {
+	pub modules:    HashMap<AtomicByteTendril, Spanned<Module, Position>>,
+	pub primitives: HashMap<AtomicByteTendril, Spanned<Primitive, Position>>,
+	pub directives: Vec<Spanned<CompilerDirective, Position>>,
+	pub comments:   Vec<Spanned<Comment, Position>>,
+}
 
 #[cfg(test)]
 mod tests {
