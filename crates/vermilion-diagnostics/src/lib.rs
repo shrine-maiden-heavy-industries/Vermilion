3b705cde6add9f22eb3ac67036bc71b8a1ed1519
dragonmux <git@dragonmux.network>
2026-01-20 13:18:56 +0000

    diagnostics: Begun building out machinary for storing diagnostics in a document at some position and for some span of it

--- crates/vermilion-diagnostics/src/lib.rs
+++ crates/vermilion-diagnostics/src/lib.rs
@@ -7,4 +7,57 @@
 
 pub mod workspace;
 
-pub struct Diagnostic {}
+/// The creature-readable contextual position within a document
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Position {
+	line:      usize,
+	character: usize,
+}
+
+#[derive(Copy, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Span<T = usize, C = ()> {
+	begin:   T,
+	end:     T,
+	context: C,
+}
+
+impl<T, C> Span<T, C> {
+	pub fn new(begin: T, end: T, context: C) -> Self {
+		Self { begin, end, context }
+	}
+}
+
+#[derive(Debug, Clone)]
+pub struct Diagnostic {
+	position: Position,
+	message:  String,
+}
+
+impl Position {
+	pub const fn sof() -> Self {
+		Self { line: 0, character: 0 }
+	}
+
+	pub const fn new(line: usize, character: usize) -> Self {
+		Self { line, character }
+	}
+}
+
+impl Diagnostic {
+	// XXX: This fails to properly convert location spans into diagnostic spans
+	pub fn new<LocationSpan, Str>(location: Option<LocationSpan>, message: Str) -> Self
+	where
+		Span<usize, Position>: From<LocationSpan>,
+		Str: Into<String>,
+		Option<Span<usize, Position>>: From<Option<LocationSpan>>,
+	{
+		let location: Option<Span<usize, Position>> = location.into();
+		let position = if let Some(span) = location {
+			span.context
+		} else {
+			Position::sof()
+		};
+
+		Self { position, message: message.into() }
+	}
+}
