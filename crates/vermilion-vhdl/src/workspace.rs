ae6ed2db361026acd66ceb3a8605d2c154bb47ab
Aki Van Ness <aki@lethalbit.net>
2026-01-29 21:46:47 -0800

    vhdl: Added `VhdlAms` config stub

--- crates/vermilion-vhdl/src/workspace.rs
+++ crates/vermilion-vhdl/src/workspace.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-use crate::{VhdlStd, fmt::workspace::VhdlFormat, lint::workspace::VhdlLint};
+use crate::{
+	VhdlAmsStd, VhdlStd,
+	fmt::workspace::{VhdlAmsFormat, VhdlFormat},
+	lint::workspace::{VhdlAmsLint, VhdlLint},
+};
 
 #[derive(Clone, Debug, Default)]
 #[cfg_attr(
@@ -13,4 +17,17 @@ pub struct VhdlConfig {
 	std:  VhdlStd,
 	fmt:  Option<VhdlFormat>,
 	lint: Option<VhdlLint>,
+}
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(
+	feature = "serde",
+	derive(::serde::Serialize, ::serde::Deserialize),
+	serde(deny_unknown_fields)
+)]
+#[cfg_attr(feature = "schema", derive(::schemars::JsonSchema))]
+pub struct VhdlAmsConfig {
+	std:  VhdlAmsStd,
+	fmt:  Option<VhdlAmsFormat>,
+	lint: Option<VhdlAmsLint>,
 }
