e86dd7844841e7fa5eaffb3cfab3155ce302109e
Aki Van Ness <aki@lethalbit.net>
2026-01-29 21:46:31 -0800

    vhdl: fmt: Added `VhdlAmsFmt` config stub

--- crates/vermilion-vhdl/src/fmt/workspace.rs
+++ crates/vermilion-vhdl/src/fmt/workspace.rs
@@ -13,3 +13,15 @@ pub struct VhdlFormat {
 	#[cfg_attr(feature = "serde", serde(flatten))]
 	common: CommonFormat,
 }
+
+#[derive(Clone, Debug, Default)]
+#[cfg_attr(
+	feature = "serde",
+	derive(::serde::Serialize, ::serde::Deserialize),
+	serde(deny_unknown_fields)
+)]
+#[cfg_attr(feature = "schema", derive(::schemars::JsonSchema))]
+pub struct VhdlAmsFormat {
+	#[cfg_attr(feature = "serde", serde(flatten))]
+	common: CommonFormat,
+}
