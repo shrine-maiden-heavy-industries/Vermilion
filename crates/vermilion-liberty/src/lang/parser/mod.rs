1193c29325f6e56df00dbcde599afe4e2c9adfab
Aki Van Ness <aki@lethalbit.net>
2026-03-01 22:28:57 -0800

    liberty: lang: parser: Stubbed out `LibertyParser` a bit

--- crates/vermilion-liberty/src/lang/parser/mod.rs
+++ crates/vermilion-liberty/src/lang/parser/mod.rs
@@ -1,5 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+use vermilion_lang::{AtomicByteTendril, Position, Spanned};
+
+use crate::{
+	LanguageStd,
+	lang::tokenizer::{LibertyTokenizer, token::Token},
+};
+
+pub struct LibertyParser {
+	_std:           LanguageStd,
+	_tokenizer:     LibertyTokenizer,
+	_current_token: Option<Spanned<Token, Position>>,
+}
+
+impl LibertyParser {
+	pub fn new(std: LanguageStd, content: AtomicByteTendril) -> eyre::Result<Self> {
+		Ok(Self {
+			_std:           std,
+			_tokenizer:     LibertyTokenizer::new(std, content)?,
+			_current_token: None,
+		})
+	}
+}
+
 #[cfg(test)]
 #[path = "tests.rs"]
 mod tests;
