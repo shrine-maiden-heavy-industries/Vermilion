2747ef3d48393bb713364cd0b40b9b19e9200455
Aki Van Ness <aki@lethalbit.net>
2026-03-08 23:33:41 -0700

    xact: schemas: ipxact ve 2022: Stubbed out modules for schema defs

---
+++ crates/vermilion-xact/src/schemas/ipxact_ve_2022/cond/mod.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+pub mod auto_configure;
+pub mod common_structures;
+pub mod is_present;
+pub mod simple_types;
--- crates/vermilion-xact/src/schemas/ipxact_ve_2022/mod.rs
+++ crates/vermilion-xact/src/schemas/ipxact_ve_2022/mod.rs
@@ -1,1 +1,13 @@
 // SPDX-License-Identifier: BSD-3-Clause
+
+pub mod abstraction_def_extensions;
+pub mod abstraction_definition;
+pub mod abstractor;
+pub mod abstractor_extensions;
+pub mod component;
+pub mod component_extensions;
+pub mod cond;
+pub mod design;
+pub mod design_config;
+pub mod design_config_extensions;
+pub mod design_extensions;
