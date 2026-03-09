c64203584c42b882d5cc7559e0088ddaa7180910
Aki Van Ness <aki@lethalbit.net>
2026-03-08 21:51:16 -0700

    xact: schemas: Stubbed out schema modules

--- crates/vermilion-xact/src/lib.rs
+++ crates/vermilion-xact/src/lib.rs
@@ -4,3 +4,5 @@
 // #![warn(missing_docs)]
 // #![warn(clippy::missing_docs_in_private_items)]
 #![deny(clippy::unwrap_used, clippy::expect_used)]
+
+pub mod schemas;
---
+++ crates/vermilion-xact/src/schemas/mod.rs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+pub mod ipxact_2009;
+pub mod ipxact_2014;
+pub mod ipxact_2022;
+pub mod ipxact_ve_2009;
+pub mod ipxact_ve_2022;
+pub mod spirit_1_0;
+pub mod spirit_1_1;
+pub mod spirit_1_2;
+pub mod spirit_1_4;
+pub mod spirit_1_5;
