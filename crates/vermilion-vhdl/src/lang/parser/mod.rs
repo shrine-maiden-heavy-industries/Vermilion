9eb25bb7682e4287d8d86bf9f57c86ca40edc19a
Aki Van Ness <aki@lethalbit.net>
2026-03-11 14:04:24 -0700

    vhdl: lang: parser: Normalized parser stubs

--- crates/vermilion-vhdl/src/lang/parser/mod.rs
+++ crates/vermilion-vhdl/src/lang/parser/mod.rs
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 use vermilion_lang::AtomicByteTendril;
+use vermilion_loc::{Position, Spanned};
 
-use crate::LanguageStd;
+use crate::{
+	LanguageStd,
+	lang::tokenizer::{VhdlTokenizer, token::Token},
+};
 
 pub mod error;
 
 pub struct VhdlParser {
-	_std:     LanguageStd,
-	_content: AtomicByteTendril,
+	_std:           LanguageStd,
+	_tokenizer:     VhdlTokenizer,
+	_current_token: Option<Spanned<Token, Position>>,
 }
 
 impl VhdlParser {
-	pub fn new(_std: LanguageStd, _content: AtomicByteTendril) -> eyre::Result<Self> {
-		Ok(Self { _std, _content })
+	pub fn new(std: LanguageStd, content: AtomicByteTendril) -> eyre::Result<Self> {
+		Ok(Self {
+			_std:           std,
+			_tokenizer:     VhdlTokenizer::new(std, content)?,
+			_current_token: None,
+		})
 	}
 }
 
