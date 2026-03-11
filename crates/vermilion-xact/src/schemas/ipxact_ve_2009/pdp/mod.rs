4aeae0dc96dfdd9697423c917a18029bfd4e7f0c
Aki Van Ness <aki@lethalbit.net>
2026-03-11 07:36:49 -0700

    xact: Added allows for `clippy::module_inception`

--- crates/vermilion-xact/src/schemas/ipxact_ve_2009/pdp/mod.rs
+++ crates/vermilion-xact/src/schemas/ipxact_ve_2009/pdp/mod.rs
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+#[allow(
+	clippy::module_inception,
+	reason = "At the whims of how the schema types are defined"
+)]
 pub mod pdp;
 pub mod view;
 pub mod wire;
--- crates/vermilion-xact/src/schemas/ipxact_ve_2009/power/mod.rs
+++ crates/vermilion-xact/src/schemas/ipxact_ve_2009/power/mod.rs
@@ -3,5 +3,9 @@
 pub mod component;
 pub mod component_instance;
 pub mod logical_wire;
+#[allow(
+	clippy::module_inception,
+	reason = "At the whims of how the schema types are defined"
+)]
 pub mod power;
 pub mod wire;
