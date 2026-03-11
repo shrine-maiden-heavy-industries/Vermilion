285dd198ddfbd7a768097626c71b8867b62e00ea
Aki Van Ness <aki@lethalbit.net>
2026-03-11 13:46:09 -0700

    diagnostics: Stubbed out rough `Diagnostic` type
    There will need to be a lot more metadata and stuff added, along with support for codes and all that jazz

--- crates/vermilion-diagnostics/src/lib.rs
+++ crates/vermilion-diagnostics/src/lib.rs
@@ -4,6 +4,33 @@
 // #![warn(missing_docs)]
 // #![warn(clippy::missing_docs_in_private_items)]
 
+use vermilion_loc::{position::Position, span::Span};
+
 pub mod workspace;
 
-pub struct Diagnostic {}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Diagnostic {
+	span:    Option<Span<usize, Position>>,
+	message: String,
+}
+
+impl Diagnostic {
+	pub fn new<Str>(message: Str, location: Option<Span<usize, Position>>) -> Self
+	where
+		Str: Into<String>,
+	{
+		Self { message: message.into(), span: location }
+	}
+
+	pub fn message(&self) -> &String {
+		&self.message
+	}
+
+	pub fn span(&self) -> Option<&Span<usize, Position>> {
+		self.span.as_ref()
+	}
+
+	pub fn position(&self) -> Option<&Position> {
+		self.span.as_ref().map(|f| f.context())
+	}
+}
