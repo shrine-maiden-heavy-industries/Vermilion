8eb2eb8072782650de8da97e8bc68f15938d02f5
Aki Van Ness <aki@lethalbit.net>
2026-03-11 07:34:21 -0700

    vhdl: Added reasons for `allow(clippy:unwrap_used)` and `allow(clippy::expect_used)`'s

--- crates/vermilion-vhdl/src/lang/tokenizer/tests.rs
+++ crates/vermilion-vhdl/src/lang/tokenizer/tests.rs
@@ -244,8 +244,10 @@ macro_rules! tokenizer_test {
 		paste! {
 			#[test]
 			fn [<test_tokenize_ $test_name>] () {
-				// SAFETY: We're in a testing context, panicking here is fine
-				#[allow(clippy::unwrap_used)]
+				#[allow(
+					clippy::unwrap_used,
+					reason = "We are within tests, and while this shouldn't explode, if it does here that's fine"
+				)]
 				let tokenizer = VhdlTokenizer::new($std, $input.as_bytes().into());
 
 				let parsed = tokenizer.collect::<Vec<_>>();
