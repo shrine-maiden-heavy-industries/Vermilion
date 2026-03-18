bec56313022edd09eb9f64c39e3793ce88f064fe
Aki Van Ness <aki@lethalbit.net>
2026-03-17 21:16:45 -0700

    liberty: Stubbed out diagnostics tables

---
+++ crates/vermilion-liberty/src/diagnostics/mod.rs
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+vermilion_diagnostics::define_diagnostics_table! {
+	info, "./messages/info", liberty,
+	0000 => "Placeholder",
+}
+
+vermilion_diagnostics::define_diagnostics_table! {
+	lint, "./messages/lint", liberty,
+	0000 => "Placeholder",
+}
+
+vermilion_diagnostics::define_diagnostics_table! {
+	warn, "./messages/warn", liberty,
+	0000 => "Placeholder",
+}
+
+vermilion_diagnostics::define_diagnostics_table! {
+	error, "./messages/error", liberty,
+	0000 => "Placeholder",
+}
+
+vermilion_diagnostics::define_diagnostics_table! {
+	debug, "./messages/debug", liberty,
+	0000 => "Placeholder",
+}
