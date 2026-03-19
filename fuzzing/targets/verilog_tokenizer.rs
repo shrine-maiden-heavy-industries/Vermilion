23555c57bd6415fe3b9bad74cc6ff0e9ff386641
Aki Van Ness <aki@lethalbit.net>
2026-03-19 09:36:24 -0700

    fuzzing: Use the new `cfg_fuzzing` macro

--- fuzzing/targets/verilog_tokenizer.rs
+++ fuzzing/targets/verilog_tokenizer.rs
@@ -1,13 +1,16 @@
-#[cfg(fuzzing)]
-use afl::fuzz;
-use vermilion_lang::AtomicByteTendril;
-use vermilion_verilog::{LanguageStd, lang::tokenizer::VerilogTokenizer};
+// SPDX-License-Identifier: BSD-3-Clause
+vermilion_macros::cfg_fuzzing! {
+	use afl::fuzz;
+	use vermilion_lang::AtomicByteTendril;
+	use vermilion_verilog::{LanguageStd, lang::tokenizer::VerilogTokenizer};
+}
 
 fn main() {
-	#[cfg(fuzzing)]
-	fuzz!(|data: &[u8]| {
-		let tokenizer =
-			VerilogTokenizer::new(LanguageStd::Vl95, AtomicByteTendril::from_slice(data)).unwrap();
-		let _ = tokenizer.collect::<Vec<_>>();
-	});
+	vermilion_macros::cfg_fuzzing! {
+		fuzz!(|data: &[u8]| {
+			let tokenizer =
+				VerilogTokenizer::new(LanguageStd::Vl95, AtomicByteTendril::from_slice(data)).unwrap();
+			let _ = tokenizer.collect::<Vec<_>>();
+		});
+	}
 }
