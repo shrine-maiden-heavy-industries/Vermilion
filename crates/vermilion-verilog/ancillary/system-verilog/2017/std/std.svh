8878b10cde19d0e516df6fbde6ee63cf208669ba
Aki Van Ness <aki@lethalbit.net>
2026-03-19 11:32:26 -0700

    verilog: ancillary: stdlib stubs

---
+++ crates/vermilion-verilog/ancillary/system-verilog/2017/std/std.svh
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+	This file is part of the IEEE 1800-2017 SystemVerilog standard library provided
+	by [Vermilion] as a supplementary file for SystemVerilog support.
+
+	NOTE: This file is API compatible with, but not a direct verbatim copy of the definition
+	as noted in IEEE 1800-2017.
+
+	[Vermilion]: https://github.com/shrine-maiden-heavy-industries/Vermilion
+*/
