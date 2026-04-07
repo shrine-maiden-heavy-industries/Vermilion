9ce07ca5bce74e09128329cb0b224d6fe5bfa8ec
Aki Van Ness <aki@lethalbit.net>
2026-04-07 05:38:48 -0700

    verilog: lang: parser: Stub out token iteration
    We're also explicitly filtering out `Token::Whitespace` and `Token::Newline`'s as we want to just to be able to get the parser hashed out and working first, and we can deal with attaching and merging whitespace to the AST later

--- crates/vermilion-verilog/src/lang/parser/mod.rs
+++ crates/vermilion-verilog/src/lang/parser/mod.rs
@@ -1,26 +1,37 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-use vermilion_lang::AtomicByteTendril;
+use vermilion_lang::{AtomicByteTendril, tokenizer::TokenizerIter};
 
 use crate::{
 	LanguageStd,
-	lang::{ast::Ast, tokenizer::VerilogTokenizer},
+	lang::{
+		ast::Ast,
+		tokenizer::{VerilogTokenizer, token::Token},
+	},
 };
 
 pub struct VerilogParser {
-	_std:       LanguageStd,
-	_tokenizer: VerilogTokenizer,
+	_std:      LanguageStd,
+	tokenizer: VerilogTokenizer,
 }
 
 impl VerilogParser {
 	pub fn new(std: LanguageStd, content: AtomicByteTendril) -> eyre::Result<Self> {
 		Ok(Self {
-			_std:       std,
-			_tokenizer: VerilogTokenizer::new(std, content)?,
+			_std:      std,
+			tokenizer: VerilogTokenizer::new(std, content)?,
 		})
 	}
 
 	pub fn parse(mut self) -> eyre::Result<Ast> {
+		// TODO(aki):
+		// Skip over whitespace for now, just so we can deal with getting the parser working,
+		// once it is, figure out how to merge in whitespace to the AST.
+		let mut iter = TokenizerIter::new(&mut self.tokenizer)
+			.filter(|token| !matches!(token.inner(), Token::Whitespace(_) | Token::Newline(_)));
+
+		for token in &mut iter {}
+
 		Ok(Ast::default())
 	}
 }
