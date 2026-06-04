463cb9cd2a9ae8e1ebeb7e540a091b50c77e297f
Aki Van Ness <aki@lethalbit.net>
2026-06-04 13:14:04 -0700

    xact: schema: xs: Add a `thin_schema` macro for simple schema cases

--- crates/vermilion-xact/src/schemas/xs/schema.rs
+++ crates/vermilion-xact/src/schemas/xs/schema.rs
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //! JSON Schema implementations for XML Schema types.
 
-impl schemars::JsonSchema for super::Double {
-	fn schema_name() -> std::borrow::Cow<'static, str> {
-		"double".into()
-	}
+macro_rules! thin_schema {
+	($type:ty, $name:literal, $desc:literal, $schema_type:literal) => {
+		impl schemars::JsonSchema for $type {
+			fn schema_name() -> std::borrow::Cow<'static, str> {
+				$name.into()
+			}
 
-	fn schema_id() -> std::borrow::Cow<'static, str> {
-		"xs::double".into()
-	}
+			fn schema_id() -> std::borrow::Cow<'static, str> {
+				concat!("xs::", $name).into()
+			}
 
-	fn json_schema(_generator: &mut schemars::SchemaGenerator) -> schemars::Schema {
-		schemars::json_schema!({
-			"description": "W3C XML Schema Double",
-			"type": "number",
-		})
-	}
+			fn json_schema(_generator: &mut schemars::SchemaGenerator) -> schemars::Schema {
+				schemars::json_schema!({
+					"description": $desc,
+					"type": $schema_type,
+				})
+			}
+		}
+	};
 }
+
+thin_schema!(super::Double, "double", "W3C XML Schema double", "number");
