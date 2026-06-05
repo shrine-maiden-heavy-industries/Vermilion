115ee5fbfb1357a242b052350c69e9ff45777051
Aki Van Ness <aki@lethalbit.net>
2026-06-04 17:06:23 -0700

    xact: schema: spirit1.0: Embed the XML Schema fragments in the doc comments for `file_type`

--- crates/vermilion-xact/src/schemas/spirit_1_0/file_type.rs
+++ crates/vermilion-xact/src/schemas/spirit_1_0/file_type.rs
@@ -4,7 +4,11 @@ use crate::schemas::xs;
 
 /// Free form file type, not yet known by SPIRIT.
 ///
-/// schema-type: `spirit:userFileType`
+/// ## XML Schema
+///
+/// ```xml
+/// <xs:element name="userFileType" type="xs:string" />
+/// ```
 #[derive(
 	Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize,
 )]
@@ -16,7 +20,39 @@ pub struct UserFileType {
 
 /// Enumerated file types known by SPIRIT.
 ///
-/// schema-type: `spirit:fileType`
+/// ## XML Schema
+///
+/// ```xml
+/// <xs:element name="fileType">
+///    <xs:simpleType>
+///      <xs:restriction base="xs:string">
+///        <xs:enumeration value="unknown"/>
+///        <xs:enumeration value="cSource"/>
+///        <xs:enumeration value="cppSource"/>
+///        <xs:enumeration value="asmSource"/>
+///        <xs:enumeration value="vhdlSource"/>
+///        <xs:enumeration value="vhdlSource-87"/>
+///        <xs:enumeration value="vhdlSource-93"/>
+///        <xs:enumeration value="verilogSource"/>
+///        <xs:enumeration value="verilogSource-95"/>
+///        <xs:enumeration value="verilogSource-2001"/>
+///        <xs:enumeration value="swObject"/>
+///        <xs:enumeration value="swObjectLibrary"/>
+///        <xs:enumeration value="vhdlBinaryLibrary"/>
+///        <xs:enumeration value="verilogBinaryLibrary"/>
+///        <xs:enumeration value="unelaboratedHdl"/>
+///        <xs:enumeration value="executableHdl"/>
+///        <xs:enumeration value="systemVerilogSource"/>
+///        <xs:enumeration value="systemVerilogSource-3.0"/>
+///        <xs:enumeration value="systemVerilogSource-3.1"/>
+///        <xs:enumeration value="systemCSource"/>
+///        <xs:enumeration value="systemCSource-2.0"/>
+///        <xs:enumeration value="veraSource"/>
+///        <xs:enumeration value="eSource"/>
+///      </xs:restriction>
+///    </xs:simpleType>
+/// </xs:element>
+/// ```
 #[derive(
 	Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize,
 )]
@@ -57,7 +93,16 @@ pub enum KnownFileType {
 
 /// The type of a file referenced by SPIRIT.
 ///
-/// schema-type: `spirit:fileType`
+/// ## XML Schema
+///
+/// ```xml
+/// <xs:group name="fileType">
+///   <xs:choice>
+///     <xs:element name="fileType" type="spirit:fileType" />
+///     <xs:element name="userFileType" type="spirit:userFileType" />
+///   </xs:choice>
+/// </xs:group>
+/// ```
 #[derive(
 	Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize,
 )]
