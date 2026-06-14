df405d807fb8adb065ba697e6942c10043bfb463
Aki Van Ness <aki@lethalbit.net>
2026-06-13 23:37:11 -0700

    alembic: Add the alembic library entrypoint

--- crates/alembic/src/main.rs
+++ crates/alembic/src/main.rs
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+//! # Alembic CLI
+//!
+//! See [alembic] for details
 
+/// Main Alembic CLI entrypoint, see [`alembic::main`] for details
 fn main() -> eyre::Result<()> {
-	Ok(())
+	alembic::main()
 }
