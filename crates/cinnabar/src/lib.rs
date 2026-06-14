ef24740b8307070b6b3232915b650b4d09b91232
Aki Van Ness <aki@lethalbit.net>
2026-06-13 23:52:54 -0700

    cinnabar: Rough CLI entrypoint machinery

--- crates/cinnabar/src/lib.rs
+++ crates/cinnabar/src/lib.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
+use eyre::{Context, OptionExt};
+
 mod cli;
 mod env;
 mod hooks;
@@ -9,5 +11,17 @@ mod hooks;
 pub fn main() -> eyre::Result<()> {
 	hooks::install()?;
 
-	Ok(())
+	let mut cli = cli::init()?;
+
+	// XXX(aki): We need to clone the Command here because we need it still
+	let args = cli.clone().get_matches();
+
+	// Try to invoke the command
+	match args.subcommand() {
+		Some((sub_cmd, args)) => cli::exec_command(sub_cmd).ok_or_eyre(format!(
+			"Unable to find command entry point for `{}`!",
+			sub_cmd
+		))?(&mut cli, args),
+		_ => cli.print_help().wrap_err("Unable to write CLI help?"),
+	}
 }
