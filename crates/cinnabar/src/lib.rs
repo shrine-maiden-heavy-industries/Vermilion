6fdf1e5acf428fc2d878f721bd0712190dc7c6a6
Aki Van Ness <aki@lethalbit.net>
2026-06-13 23:44:14 -0700

    cinnabar: Add eyre an panic hook install

--- crates/cinnabar/src/lib.rs
+++ crates/cinnabar/src/lib.rs
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
+mod hooks;
+
 /// Main entrypoint for `Cinnabar`
 pub fn main() -> eyre::Result<()> {
+	hooks::install()?;
+
 	Ok(())
 }
