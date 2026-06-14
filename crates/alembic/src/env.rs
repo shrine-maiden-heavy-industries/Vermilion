80e8d4dfa64135ebd64a8fce4466e0c5006b2a0e
Aki Van Ness <aki@lethalbit.net>
2026-06-13 23:51:22 -0700

    alembic: Add `env` module

---
+++ crates/alembic/src/env.rs
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+pub static ALEMBIC_LOG_LEVEL: &str = "ALEMBIC_LOG_LEVEL";
--- crates/alembic/src/lib.rs
+++ crates/alembic/src/lib.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
+mod env;
 mod hooks;
 
 /// Main entrypoint for `Alembic`
