9d71d15a6246d8578441c4333677e9c73c838db6
Aki Van Ness <aki@lethalbit.net>
2026-06-13 23:36:52 -0700

    cinnabar: Add the cinnabar library entrypoint

---
+++ crates/cinnabar/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: BSD-3-Clause
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+
+/// Main entrypoint for `Cinnabar`
+pub fn main() -> eyre::Result<()> {
+	Ok(())
+}
--- crates/cinnabar/src/main.rs
+++ crates/cinnabar/src/main.rs
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+//! # Cinnabar CLI
+//!
+//! See [cinnabar] for details
 
+/// Main Cinnabar CLI entrypoint, see [`cinnabar::main`] for details
 fn main() -> eyre::Result<()> {
-	Ok(())
+	cinnabar::main()
 }
