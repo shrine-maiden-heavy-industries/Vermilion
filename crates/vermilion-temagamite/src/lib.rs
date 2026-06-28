3a4373276a3c025eb2e65343f0c7baa8a1a932b5
Aki Van Ness <aki@lethalbit.net>
2026-06-28 07:20:57 -0700

    temagamite: Version enum

--- crates/vermilion-temagamite/src/lib.rs
+++ crates/vermilion-temagamite/src/lib.rs
@@ -7,3 +7,20 @@
 
 pub mod config;
 pub mod workspace;
+
+pub enum Version {
+	/// Conforms to the SystemVerilog and VHDL LRMs with tool-specific interpretation of the use
+	/// model and pragma syntax.
+	///
+	/// There is no expectation of interoperability.
+	///
+	/// This version only exists to support backward compatibility to older implementations.
+	Zero,
+	/// Conforms to IEEE 1735 basic interoperability specifications, which are important both
+	/// because they allow basic interoperability of protected IP across a set of tools and because
+	/// there is already defacto conformance to them at this time.
+	One,
+	/// Conforms to all IEEE 1735 recommendations for use model and pragmas, including all Version 1
+	/// requirements.
+	Two,
+}
