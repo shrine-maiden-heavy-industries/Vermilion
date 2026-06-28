136a3314a76fa70a647c12b39e99c7a3c1e7f487
Aki Van Ness <aki@lethalbit.net>
2026-06-28 07:41:48 -0700

    temagamite: blocks: Common/Tool block stubs

--- crates/vermilion-temagamite/src/blocks.rs
+++ crates/vermilion-temagamite/src/blocks.rs
@@ -1,1 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
+
+use std::borrow::Cow;
+
+pub struct CommonBlock {}
+
+pub struct ToolBlock<'a> {
+	pub(crate) key_owner: Cow<'a, str>,
+	pub(crate) key_name:  Cow<'a, str>,
+}
